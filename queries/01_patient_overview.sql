aba382558db6b048f9e6bb26a2e5b9dc8c1d846b
MahsaFarnia23 <143577665+MahsaFarnia23@users.noreply.github.com>
2025-11-18 13:07:35 -0500

    Add SQL queries for patient overview analysis

--- queries/01_patient_overview.sql
+++ queries/01_patient_overview.sql
@@ -1,1 +1,25 @@
+--number of patients by city
+select distinct city, count(city) as total_patient from patients
+group by city 
+order by city DESC
+
+
+--avarage of patient's age per insurance provider
+select distinct insurance_provider, avg(age) as avg_age from patients
+group by insurance_provider
+
+
+--patient's name with more than 3 appointments in two last years
+SELECT 
+    p.patient_id,
+    p.name,
+    COUNT(a.appointment_id) AS appointment_count
+FROM patients AS p
+JOIN appointments AS a
+    ON p.patient_id = a.patient_id
+WHERE a.appointment_date >= date('now', '-2 year')
+GROUP BY p.patient_id, p.name
+HAVING COUNT(a.appointment_id) > 3
+ORDER BY appointment_count DESC;
+
 
